4d981983520343adf698f9c818bad90adb19cc0e
Ekzog <andruuusha2000@gmail.com>
2024-05-13 08:52:39 +0700

    indev: add enemy iq

--- scripts/gameObject.js
+++ scripts/gameObject.js
@@ -276,22 +276,193 @@ function Camera() {
     return camera;
 }
 
-function Enemy(position, map) {
-    let enemy = GameObject(position, Vector2D(100,100),"red");
+function twoclose(data, number){
+    var closestLeft,
+        closestRight,
+        current;
+      for (var i = 0; i < data.length; i++) {
+          current = data[i];
+          if (current < number && (typeof closestLeft === 'undefined' || closestLeft < current)) {
+              closestLeft = current;
+          } else if (current > number && (typeof closestRight === 'undefined' || closestRight > current)) {
+              closestRight = current;
+          }
+      }
+      /*
+      if(closestRight === undefined) return [closestLeft];
+      else if(closestLeft === undefined) return [closestRight];
+      else*/ return [closestLeft, closestRight];
+    }
+    
+    function creatGraph(map){
+            var graph = new Object();
+    
+        for(let i = 0; i < map.length; i++){
+          var point = map[i];
+          var array_x = [];
+          var array_y = [];
+          var points = [];
+          var ways = [];
+            for(let j = 0; j < map.length; j++){
+              if(i != j){
+              if(point.x == map[j].x || point.y == map[j].y){
+                points.push(map[j]);
+                if(point.x == map[j].x){
+                  array_x.push(map[j].y);
+                } 
+                if(point.y == map[j].y){
+                  array_y.push(map[j].x);
+                }
+                }
+              }
+            }
+            //console.log(JSON.stringify(point));
+            if(array_x.length > 0){
+              //console.log(i, point.x , array_x, 'Ближайшие по x слева', twoclose(array_x, point.x)[0], 'Ближайшие по x справа', twoclose(array_x, point.x)[1]);
+                for(let i = 0; i < points.length; i++){
+                  if(points[i].y == twoclose(array_x, point.x)[0] || points[i].y == twoclose(array_x, point.x)[1]) ways.push('{"x":'+points[i].x+',"y":'+points[i].y+'}'); //ways.push(points[i]);
+                }
+              } 
+            if(array_y.length > 0){
+              //console.log(i, point.y, array_y, 'Ближайшие по y слева', twoclose(array_y, point.y)[0],  'Ближайшие по y справа', twoclose(array_y, point.y)[1]);
+              for(let i = 0; i < points.length; i++){
+                if(points[i].x == twoclose(array_y, point.y)[0] || points[i].x == twoclose(array_y, point.y)[1]) ways.push('{"x":'+points[i].x+',"y":'+points[i].y+'}');
+              }
+            }
+            point_text = '{"x":'+point.x+',"y":'+point.y+'}';
+            graph[point_text] = ways;
+            //console.log(JSON.stringify(ways));
+        }
+        //console.log(graph);
+    return graph;
+    }
+    
+    function bfs(graph, start, end) {
+        console.log(graph);
+        let queue = [[start, []]],
+            seen = new Set;
+    
+        while (queue.length) {
+            let [curVert, [...path]] = queue.shift();
+            path.push(curVert);
+            if (curVert === end) return path;
+    
+            if (!seen.has(curVert) && graph[curVert]) {
+                queue.push(...graph[curVert].map(v => [v, path]));
+            }
+            seen.add(curVert);
+        }
+        //return queue;
+    }
+
+function Enemy(position, enemy_map) {
+    let enemy = GameObject(position, Vector2D(50,200),"red");
     //enemy.damage = true;
     enemy.walkSpeed = 0.05;
     enemy.maxWalkTime = 1000;
     enemy.layer = 2;
     enemy.time = 0;
     enemy.name = "enemy";
-
+    enemy.texture_size = Vector2D(256, 256);
+    enemy.map = [];
+    for(let i = 0; i< enemy_map.length; i+=1){
+        for(let j = 0; j< enemy_map[i].length; j+=1){
+            if(enemy_map[i][j] == 1) enemy.map.push(Vector2D(j * 100 + 50, i * 100));
+        }
+    }
+    //console.log(player.position);
+    //console.log(enemy.map);
+    var timer = 0;
+    var road = [];
+    var step = 0;
     enemy.onUpdate = deltaTime => {
+        if(Math.abs(player.position.x - enemy.position.x) > 0 || Math.abs(player.position.y - enemy.position.y) > 0){
+            enemy.velocity.x = 0;
+            if(timer == 200){
+                console.log('Прошла секунда');
+                timer = 0;
+                var enemy_map = enemy.map;
+                var enemy_position = {
+                    x: enemy.position.x,
+                    y: enemy.position.y + 100
+                };
+                var player_position = {
+                    x: player.position.x,
+                    y: player.position.y + 50
+                };
+                enemy_map.push(enemy_position);
+                enemy_map.push(player_position);
+                var start_point = '{"x":' + enemy_position.x + ',"y":' + enemy_position.y + '}';
+                var end_point = '{"x":' + player_position.x + ',"y":' + player_position.y + '}';
+                road = bfs(creatGraph(enemy_map), start_point, end_point);
+                //for(let i=0; i<road.length; i++) console.log(JSON.parse(road[i]));
+                console.log(start_point);
+                console.log(end_point);
+                step = 0;
+                //console.log(road);
+            }
+            //if(road.length > 0 )console.log(road.length);
+            if(road !== undefined){
+                if(road.length > 0 && step < road.length){
+                    console.log(step, JSON.parse(road[step]).y , enemy.position.y);
+                    if(JSON.parse(road[step]).x < enemy.position.x){
+                        enemy.velocity.x = -enemy.walkSpeed;
+                    } 
+                    if(JSON.parse(road[step]).x > enemy.position.x){
+                        enemy.velocity.x = +enemy.walkSpeed;
+                    } 
+                    if(JSON.parse(road[step]).x == enemy.position.x){
+                        //enemy.g = 0.004;
+                        if(JSON.parse(road[step]).y == (enemy.position.y + 100)) step++;
+                        else {
+                            enemy.g = 0;
+                            if(JSON.parse(road[step]).y < (enemy.position.y + 100)){
+                                enemy.position.y -= 1;
+                                console.log('Нужно вверх');
+                            }
+                            if(JSON.parse(road[step]).y > (enemy.position.y + 100)){
+                                enemy.position.y += 1;
+                                console.log('Нужно вниз');
+                            }
+                        }
+                    }
+                }
+            }
+            //console.log(road);
+            timer++;
+            /*
+            var enemy_map = enemy.map;
+            var enemy_position = {
+                x: enemy.position.x,
+                y: enemy.position.y + 100
+            };
+            var player_position = {
+                x: player.position.x,
+                y: player.position.y + 100
+            };
+            enemy_map.push(enemy_position);
+            enemy_map.push(player_position);
+            var start_point = '{"x":' + enemy_position.x + ',"y":' + enemy_position.y + '}';
+            var end_point = '{"x":' + player_position.x + ',"y":' + player_position.y + '}';
+            var road = bfs(creatGraph(enemy_map), start_point, end_point);
+            //for(let i=0; i<road.length; i++) console.log(JSON.parse(road[i]));
+            console.log(start_point);
+            console.log(end_point);
+            console.log(road);
+            */
+        }
+        /*
         if((player.position.x - enemy.position.x) > 0){
+            enemy.texture_position = Vector2D(0, 0);
+            enemy.img.src = "resources/enemy/enemy_run_right_1.png";
             enemy.velocity.x = enemy.walkSpeed;   
         }
         if((player.position.x - enemy.position.x) < 0){
+            enemy.texture_position = Vector2D(0, 0);
+            enemy.img.src = "resources/enemy/enemy_run_left_1.png";
             enemy.velocity.x = -enemy.walkSpeed;   
         }
+        */
         
         //console.log('Врага',enemy.position.x, ' ', enemy.position.y);
     }
--- scripts/main.js
+++ scripts/main.js
@@ -43,46 +43,87 @@ window.addEventListener('load', () => {
     canvas.getContext('2d').drawImage(img_map, 0, 0, img_map.width, img_map.height);
     var pixelData = canvas.getContext('2d').getImageData(0, 0, img_map.width, img_map.height).data;
     var map = new Array(img_map.height);
-    map = [1];
     
     for(let i = 0; i< img_map.height; i+=1){
         map[i] = new Array(img_map.width);
         for(let j = 0; j< img_map.width; j+=1){
             var pixel = [pixelData[j * 4 + 0 + (img_map.width * 4 * i)], pixelData[j * 4 + 1 + (img_map.width * 4 * i)], pixelData[j * 4 + 2 + (img_map.width * 4 * i)]];
-            //map[i][j] = 1;
-            //console.log(pixel);
-            if(JSON.stringify(pixel) == JSON.stringify([0, 0, 0])) map[i-1][j] = 1;
-            if(JSON.stringify(pixel) == JSON.stringify([255, 0, 255])) map[i-1][j] = 1;
-            if(JSON.stringify(pixel) == JSON.stringify([0, 255, 255])) map[i-1][j] = 1;
+            map[i][j] = pixel;
         }
     }
-    //console.log(map);
+    var virtual_map = new Array(map.length);
     for(let i = 0; i< map.length; i+=1){
+        virtual_map[i] = new Array(map[i].length);
         for(let j = 0; j< map[i].length; j+=1){
-            if(map[i][j] == 1){
-                Way(Vector2D(j * 100, i * 100), Vector2D(100, 100));
-                //console.log('Поставил');
+            if(JSON.stringify(map[i][j]) == JSON.stringify([0, 0, 0])) virtual_map[i][j] = 1; //Branch(Vector2D(j * 100, i * 100), Vector2D(100, 100));
+            if(JSON.stringify(map[i][j]) == JSON.stringify([150, 100, 70])) virtual_map[i][j] = 1; //Earth(Vector2D(j * 100, i * 100), Vector2D(100, 100));
+            if(JSON.stringify(map[i][j]) == JSON.stringify([80, 50, 30])) virtual_map[i][j] = 1; //Earth_floor(Vector2D(j * 100, i * 100), Vector2D(100, 100));
+            if(JSON.stringify(map[i][j]) == JSON.stringify([60, 40, 20])) virtual_map[i][j] = 1; //Earth_left(Vector2D(j * 100, i * 100), Vector2D(100, 100));
+            if(JSON.stringify(map[i][j]) == JSON.stringify([40, 20, 10])) virtual_map[i][j] = 1;//Earth_right(Vector2D(j * 100, i * 100), Vector2D(100, 100));
+            if(JSON.stringify(map[i][j]) == JSON.stringify([20, 10, 5])) virtual_map[i][j] = 1;//Earth_top(Vector2D(j * 100, i * 100), Vector2D(100, 100));
+            if(JSON.stringify(map[i][j]) == JSON.stringify([255, 0, 255])) virtual_map[i][j] = 2;  //Ladder(Vector2D(j * 100, i * 100), Vector2D(100, 100));
+            if(JSON.stringify(map[i][j]) == JSON.stringify([0, 255, 255])) virtual_map[i][j] = 3;//End_Ladder(Vector2D(j * 100, i * 100-10), Vector2D(100, 100));
+        }
+    }
+
+    //console.log(map);
+    var enemy_map = new Array(virtual_map.length);
+    for(let i = 0; i< virtual_map.length; i+=1){
+        enemy_map[i] = new Array(virtual_map[i].length);
+        for(let j = 0; j< virtual_map[i].length; j+=1){
+            
+            if(virtual_map[i][j] == 3){
+                enemy_map[i-1][j] = 1;
             } 
+            if((virtual_map[i][j] == 2) && (virtual_map[i+1][j] == 1)){
+                enemy_map[i][j] = 1;
+            }
+            
+            if((virtual_map[i][j] == 1) && (virtual_map[i-1][j] == 1) && (virtual_map[i][j+1] == 1)){
+                console.log('Есть контакт');
+                enemy_map[i-1][j] = 1;
+            }
+
+            if((virtual_map[i][j] == 1) && (virtual_map[i-1][j] != 1) && (virtual_map[i][j+1] != 1)){
+                console.log('Есть контакт');
+                enemy_map[i-1][j] = 1;
+            }
+
+            if((virtual_map[i][j] == 1) && (virtual_map[i-1][j] != 1) && (virtual_map[i][j-1] != 1)){
+                console.log('Есть контакт');
+                enemy_map[i-1][j-1] = 1;
+            }
+
+            if((virtual_map[i][j] == 1) && (virtual_map[i-1][j] == 1) && (virtual_map[i][j-1] == 1)){
+                console.log('Есть контакт');
+                enemy_map[i-1][j-1] = 1;
+            }
+        }
+    }
+
+    for(let i = 0; i< enemy_map.length; i+=1){
+        for(let j = 0; j< enemy_map[i].length; j+=1){
+            if(enemy_map[i][j] == 1) Way(Vector2D(j * 100 + 50, i * 100), Vector2D(100, 100));
         }
     }
     
-    for(let i = 0; i< img_map.height; i+=1){
-        for(let j = 0; j< img_map.width; j+=1){
-            var pixel = [pixelData[j * 4 + 0 + (img_map.width * 4 * i)], pixelData[j * 4 + 1 + (img_map.width * 4 * i)], pixelData[j * 4 + 2 + (img_map.width * 4 * i)]];
-            if(JSON.stringify(pixel) == JSON.stringify([0, 0, 255])) player = Player(Vector2D(j * 100, i * 100));
-            if(JSON.stringify(pixel) == JSON.stringify([0, 0, 0])) Branch(Vector2D(j * 100, i * 100), Vector2D(100, 100));
-            if(JSON.stringify(pixel) == JSON.stringify([255, 0, 255])) Ladder(Vector2D(j * 100, i * 100), Vector2D(100, 100));
-            if(JSON.stringify(pixel) == JSON.stringify([0, 255, 255])) End_Ladder(Vector2D(j * 100, i * 100-10), Vector2D(100, 100));
-            if(JSON.stringify(pixel) == JSON.stringify([255, 0, 0])) Enemy(Vector2D(j * 100, i * 100), map);
-            if(JSON.stringify(pixel) == JSON.stringify([255, 255, 0])) Wind(Vector2D(j * 100, i * 100), Vector2D(100, 100));
+    for(let i = 0; i< map.length; i+=1){
+        for(let j = 0; j< map[i].length; j+=1){
+            //var pixel = [pixelData[j * 4 + 0 + (img_map.width * 4 * i)], pixelData[j * 4 + 1 + (img_map.width * 4 * i)], pixelData[j * 4 + 2 + (img_map.width * 4 * i)]];
+            if(JSON.stringify(map[i][j]) == JSON.stringify([0, 0, 255])) player = Player(Vector2D(j * 100, i * 100));
+            if(JSON.stringify(map[i][j]) == JSON.stringify([0, 0, 0])) Branch(Vector2D(j * 100, i * 100), Vector2D(100, 100));
+            if(JSON.stringify(map[i][j]) == JSON.stringify([255, 0, 255])) Ladder(Vector2D(j * 100, i * 100), Vector2D(100, 100));
+            if(JSON.stringify(map[i][j]) == JSON.stringify([0, 255, 255])) End_Ladder(Vector2D(j * 100, i * 100-10), Vector2D(100, 100));
+            if(JSON.stringify(map[i][j]) == JSON.stringify([255, 0, 0])) Enemy(Vector2D(j * 100, i * 100), enemy_map);
+            if(JSON.stringify(map[i][j]) == JSON.stringify([255, 255, 0])) Wind(Vector2D(j * 100, i * 100), Vector2D(100, 100));
         
             // Здесь элементы которые добавил Влад //
 
-            if(JSON.stringify(pixel) == JSON.stringify([150, 100, 70])) Earth(Vector2D(j * 100, i * 100), Vector2D(100, 100));
-            if(JSON.stringify(pixel) == JSON.stringify([80, 50, 30])) Earth_floor(Vector2D(j * 100, i * 100), Vector2D(100, 100));
-            if(JSON.stringify(pixel) == JSON.stringify([60, 40, 20])) Earth_left(Vector2D(j * 100, i * 100), Vector2D(100, 100));
-            if(JSON.stringify(pixel) == JSON.stringify([40, 20, 10])) Earth_right(Vector2D(j * 100, i * 100), Vector2D(100, 100));
-            if(JSON.stringify(pixel) == JSON.stringify([20, 10, 5])) Earth_top(Vector2D(j * 100, i * 100), Vector2D(100, 100));
+            if(JSON.stringify(map[i][j]) == JSON.stringify([150, 100, 70])) Earth(Vector2D(j * 100, i * 100), Vector2D(100, 100));
+            if(JSON.stringify(map[i][j]) == JSON.stringify([80, 50, 30])) Earth_floor(Vector2D(j * 100, i * 100), Vector2D(100, 100));
+            if(JSON.stringify(map[i][j]) == JSON.stringify([60, 40, 20])) Earth_left(Vector2D(j * 100, i * 100), Vector2D(100, 100));
+            if(JSON.stringify(map[i][j]) == JSON.stringify([40, 20, 10])) Earth_right(Vector2D(j * 100, i * 100), Vector2D(100, 100));
+            if(JSON.stringify(map[i][j]) == JSON.stringify([20, 10, 5])) Earth_top(Vector2D(j * 100, i * 100), Vector2D(100, 100));
         }
     }
     
